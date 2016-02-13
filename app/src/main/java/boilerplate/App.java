07ac284d41ba08b59a1b8205de83bc6d21ef3fc5
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2016-02-13 20:53:39 +0200

    add application

---
+++ app/src/main/java/boilerplate/App.java
@@ -0,0 +1,15 @@
+package boilerplate;
+
+import android.app.Application;
+
+/**
+ * ~ ~ ~ ~ Description ~ ~ ~ ~
+ *
+ * @author Dmitriy Zaitsev
+ * @since 2016-Feb-13, 19:56
+ */
+public final class App extends Application {
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
