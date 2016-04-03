7a1415a7fe142e1cff9e03f9c0fed071ff676656
Dmitriy Zaitsev <zaitsev.dl@gmail.com>
2016-04-03 20:43:11 +0300

    add Flow library with some basic classes

--- presentation/src/main/java/boilerplate/presentation/view/MainActivity.java
+++ presentation/src/main/java/boilerplate/presentation/view/MainActivity.java
@@ -1,10 +1,21 @@
 package boilerplate.presentation.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import boilerplate.R;
+import boilerplate.presentation.view.flow.MyKeyChanger;
+import flow.Flow;
+import flow.KeyDispatcher;
 
 public class MainActivity extends AppCompatActivity {
+
+  @Override protected void attachBaseContext(final Context newBase) {
+    super.attachBaseContext(Flow.configure(newBase, this)
+        .dispatcher(KeyDispatcher.configure(this, new MyKeyChanger(this))
+            .build())
+        .install());
+  }
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
---
+++ presentation/src/main/java/boilerplate/presentation/view/flow/Screens.java
@@ -0,0 +1,16 @@
+package boilerplate.presentation.view.flow;
+
+import lombok.Value;
+
+/**
+ * ~ ~ ~ ~ Description ~ ~ ~ ~
+ *
+ * @author Dmitriy Zaitsev
+ * @since 2016-Apr-03, 20:13
+ */
+public final class Screens {
+
+  public static final @Value class Details {}
+
+  public static final @Value class Main {}
+}
